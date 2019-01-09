c6c4b9a5566ca037515a70436c2b3cdb058af904
Ed Bentinck <edbentinck@gmail.com>
2019-01-09 01:58:03 -0600

    Add STPKey trait

---
+++ src/STPKey/STPKeyTrait.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Kinedu\STPTransfers\STPKey;
+
+trait STPKeyTrait
+{
+    /**
+     * Generate an STP key for the current student.
+     *
+     * @return bool
+     */
+    public function generateSTPKey()
+    {
+        return true;
+    }
+}
