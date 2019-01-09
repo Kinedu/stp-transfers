d14fba52eb998f34217db812522f57dd9f8f4784
Ed Bentinck <edbentinck@gmail.com>
2019-01-08 19:30:01 -0600

    Set up package files

---
+++ src/STPTransfersServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Kinedu\STPTransfers;
+
+use Illuminate\Support\ServiceProvider;
+
+class STPTransfersServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        if (! class_exists('CreateStudentSTPDetailsTable')) {
+            $this->publishes([
+                __DIR__.'/../database/migrations/create_student_stp_details_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_student_stp_details_table.php'),
+            ], 'migrations');
+        }
+
+        if (! class_exists('CreateSTPTransactionsTable')) {
+            $this->publishes([
+                __DIR__.'/../database/migrations/create_stp_transactions_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_stp_transactions_table.php'),
+            ], 'migrations');
+        }
+    }
+}
